28e9567ca861e69e339631017f779244200e447a
Atashi-kvn <ataskv.gov@gmail.com>
2023-02-16 15:10:24 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Prints A new line, to standard error.
+ * main - Prints a new line, to standard error.
  *
- * Return: Always 1. (successful)
+ * Return: Always 1.
  */
 
 int main(void)
 {
-	fput(stdout, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
 	return (1);
 }
