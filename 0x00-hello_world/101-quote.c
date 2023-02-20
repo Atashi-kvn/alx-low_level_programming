a1740351d8c0a9d8beecea0dd894de853f8ca143
Atashi-kvn <ataskv.gov@gmail.com>
2023-02-20 04:43:58 -0800

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
 /**
- * main - Prints a new line, to standard error.
+ * main - Entry point of the program
  *
- * Return: Always 1.
+ * Return: Always 1 (Error)
  */
 
 int main(void)
 {
- const char* message = "and that piece of art is useful\" - Dora Korper, 2015-10-19\n";
- write(STDERR_FILENO, message, strlen(message));
+ const char *message = "and that piece of art is useful\" - Dora Korper, 2015-10-19\n";
+ write(STDERR_FILENO, message, 31);
 	return (1);
 }
