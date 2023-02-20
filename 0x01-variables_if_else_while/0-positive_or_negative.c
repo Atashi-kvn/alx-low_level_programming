1fc67a675434136485904b74e6481f53afdbf116
Atashi-kvn <ataskv.gov@gmail.com>
2023-02-20 04:58:09 -0800

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Printing the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
- *
- * Return: Always 0.
- */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/*the coditions for logical choice*/
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
